b56bf16e30f288e429bfb41111938d31fa18a131
Grant Klein <grant.k@eagles.oc.edu>
2025-10-20 15:21:24 -0500

    new commit

--- scripts/board.js
+++ scripts/board.js
@@ -68,7 +68,7 @@ function sendBoardToAI(boardData, retryCount = 0) {
 
     
     if (retryCount > 0) {
-        prompt += `Already have attempted move. Try something different.`;
+        prompt += `Already have attempted previous move. Try something different that is still valid.`;
     }
 
     fetch("https://chessbros.onrender.com/analyze", {
